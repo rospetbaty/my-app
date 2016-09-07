e73bf22bb0608082520340c7074d59824a8a08a8
rospetbaty <petros@sourced.tech>
2016-09-07 15:55:20 +0200

    Todo app

---
+++ src/App.js
@@ -0,0 +1,69 @@
+var React = require ('react');
+var ReactDOM = require ('react-dom');
+import logo from './logo.svg';
+import './App.css';
+
+var TodoItems = React.createClass({
+  render: function() {
+
+    var todoEntries = this.props.entries;
+
+   function createTasks(item) {
+     return <li key={item.key}>{item.text}</li>
+   }
+
+   var listItems = todoEntries.map(createTasks);
+   return (
+      <ul className="theList">
+        {listItems}
+      </ul>
+    );
+
+  }
+});
+
+var TodoList = React.createClass({
+  getInitialState: function() {
+    return {
+      items: []
+    };
+  },
+  addItem: function(e) {
+    var itemArray = this.state.items;
+
+      itemArray.push(
+        {
+          text: this._inputElement.value,
+          key: Date.now()
+        }
+      );
+
+      this.setState({
+        items: itemArray
+      });
+
+      this._inputElement.value = "";
+
+      e.preventDefault();
+    },
+  render: function() {
+      return (
+        <div className="todoListMain">
+          <div className="header">
+          <h1>App for the gifted ones!</h1>
+            <form onSubmit={this.addItem}>
+            <input ref={(a) => this._inputElement = a}
+                   placeholder="enter task">
+            </input>
+              <button type="submit">add</button>
+            </form>
+          </div>
+          <TodoItems entries={this.state.items}/>
+        </div>
+      );
+    }
+});
+
+
+
+module.exports = TodoList;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+var TodoList = require('./App');
+import './index.css';
+
+ReactDOM.render(
+  <TodoList />,
+  document.getElementById('root')
+);
